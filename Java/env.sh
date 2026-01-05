4bce1b7c4825b9add30c9e2ce57ec92edb03acc0
kystor <hcwinsjames@outlook.com>
2026-01-05 10:39:12 +0800

    Add files via upload

--- Java/env.sh
+++ Java/env.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# 文件名: jarstart.sh
-# 编码: 建议 UTF-8 (但在 Java 代码中我做了兼容读取)
+# 文件名: env.sh
 
-# 你的哪吒指令
+# 1. 哪吒指令 (这一行没问题，保持原样)
 export NEZHA_COMMAND=""
 
-# 你的业务端口
-export hypt="你的端口号"
+# 2. 业务参数 (👇 必须拆开写，一行一个)
+export vmpt=""
+export argo=""
+export agn=""
+export agk=""
+export hypt=""
