7a4a95733b9a1112b8ef644478abcccd92b70b6a
kystor <hcwinsjames@outlook.com>
2026-04-21 01:25:13 +0800

    Create env.sh

---
+++ Java/plugins/env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 文件名: env.sh
+
+# 1. 哪吒指令 (这一行没问题，保持原样)
+export NEZHA_COMMAND=""
+
+# 2. 业务参数 (👇 必须拆开写，一行一个)
+export vmpt=""
+export argo=""
+export agn=""
+export agk=""
+export hypt=""
