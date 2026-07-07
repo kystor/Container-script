8bcf86f9025425414562bd36534ebae7f2900b7a
kystor <hcwinsjames@outlook.com>
2026-07-07 23:59:12 +0800

    Add files via upload

---
+++ komari/java/env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 文件名: env.sh
+
+# 1. 哪吒指令 (这一行没问题，保持原样)
+export NEZHA_COMMAND=""
+
+# 2. Komari 指令，可填写官方安装命令或 -e/--endpoint 参数
+export KOMARI_COMMAND=""
+
+# 3. 业务参数 (👇 必须拆开写，一行一个)
+export vmpt=""
+export argo=""
+export agn=""
+export agk=""
+export hypt=""
